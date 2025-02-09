17084138de5821a99ce112a3a9bdc9d4cd275449
CristoYisus69 <crisnuva1427@gmail.com>
2025-02-09 17:34:00 -0600

    listo para el pull

---
+++ Frutos_del_Terraba/Controllers/ProductosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Frutos_del_Terraba.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+    }
+}
